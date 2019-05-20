1a45e20072d27d6549611bc2f237f7060a691c7e
ElenaSkrebneva <45042290+ElenaSkrebneva@users.noreply.github.com>
2019-05-20 21:11:32 +0300

    сборка
    изменен файл gulpfile.js согласно лекции. Сборка осуществляется командой npm start, но на комманду npm run build почему-то не отвечают функции copy clean, хотя в функции start те же функции работают. Сдаю как есть, потому что не знаю уже что с эим делать и ждать ответа куратора уже нет времени.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,49 @@
 "use strict";
 
 var gulp = require("gulp");
+var clean = require("del");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var server = require("browser-sync").create();
+
+gulp.task("images", function() {
+  return gulp.src("srouce/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 80}))
+  .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("clean", function() {
+  return clean("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,14 +53,19 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("refresh", function(done) {
+  server.reload();
+  done();
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +73,9 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("build", gulp.series("clean", "copy", "css"));
+gulp.task("start", gulp.series("build", "server"));
